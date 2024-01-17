d7aeefa661d91cdecf2277f77670df2c0ffb1b4d
dokkaner <dbzdokkaner@gmail.com>
2024-01-17 13:46:08 +0100

    fix loader indicator placement in manga.vue

--- app/src/views/manga.vue
+++ app/src/views/manga.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="fixed bottom-0 right-0 z-50 h-16 w-16">
-    <svg v-show="storeIsLoading" class="sticky h-5 w-5 animate-spin text-light-900"
+  <div v-show="storeIsLoading" class="fixed bottom-8 right-0 z-50 h-8 w-8">
+    <svg class="sticky h-5 w-5 animate-spin text-light-900 dark:text-darkAccent-400"
          xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
       <path class="opacity-75 " fill="currentColor"
             d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
